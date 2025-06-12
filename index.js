0b5c171414c38ecb66d55e06a1eecccfa41088a9
JDSwan11995 <jdswan1995@gmail.com>
2025-06-12 14:53:52 -0500

    updated mongoose connection

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 mongoose
-  .connect("mongodb://localhost:27017/ghibliDataBase")
+  .connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology:true})
   .then(() => console.log("Connected to the MongoDB Database."))
   .catch((err) => console.error("Connection Error", err));
 
@@ -719,3 +719,4 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
   console.log("Your app is listening on port " + port);
 });
+
