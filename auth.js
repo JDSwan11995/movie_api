4a81cbee29d7f01f26e62a2b640d05cb35176091
JDSwan11995 <jdswan1995@gmail.com>
2025-06-23 16:40:03 -0500

    ...

--- auth.js
+++ auth.js
@@ -16,10 +16,11 @@ let generateJWTToken = (user) => {
 /*POST login*/
 module.exports = (router) => {
     router.post('/login', (req, res) => {
+        try {
         console.log('[LOGIN] Login request received: ', req.body);
         passport.authenticate('local', {session: false}, (error, user, info) => {
             if (error || !user) {
-                console.error("[LOGIN] Passport error: ", error);
+                console.error("[LOGIN] Passport error: ", error, info);
                 return res.status(400).json({
                     message: "Something's not right, check the entry and try again.",
                     user: user,
@@ -31,12 +32,16 @@ module.exports = (router) => {
             req.login(user, {session: false}, (error) =>{
                 if (error) {
                     console.error("[LOGIN] req.login() error:", error)
-                    return res.status(500).json({message: "Login error", error});
+                    return res.status(500).send("Login error");
                 }
-                const token = generateJWTToken(user.toJSON());
+                let token = generateJWTToken(user.toJSON());
                 console.log("[LOGIN] JWT Token Generated");
                 return res.json({user, token});
             });
         })(req, res);
+    } catch (e) {
+        console.error("Something unexpected while logging in:", e);
+        res.status(503).send("Server error in /login.");
+    }
     });
 };
